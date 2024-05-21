3b47959162e9b6fc8526bd77a5efb0d05ede758f
ossama <ossamachalhaoui1@gmail.com>
2024-05-21 13:18:04 +0100

    cin

---
+++ newfile/cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v;
+    cout << "enter your age!" << endl;
+    cin>>v;
+    if (v>=18)
+    {
+    cout<<"your accepte"<<endl;
+    }else{
+        cout<<"you are rejected"<<endl;
+    }
+    return 0;
+}
