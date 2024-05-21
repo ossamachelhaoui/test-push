cbe700ce962f765685ba26d49e1d928df9c0605d
ossama <ossamachalhaoui1@gmail.com>
2024-05-21 13:01:45 +0100

    cinhomwoork

---
+++ cinhomwoork.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v;
+    cout << "enter your age!" << endl;
+    cin>>v;
+    if (v>=18){
+    cout<<"your accepted"<<endl;
+    }else{
+        cout<<"you are rejected"<<endl;
+    }
+    return 0;
+}
