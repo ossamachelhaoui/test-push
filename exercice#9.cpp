919ced2cea4c5ab51308ac4a20f921d98c2c630e
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 11:57:33 +0100

    #9

---
+++ exercice#9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int x;
+    int y;
+    int z;
+
+ cout<<"enter the first number :"<<endl;
+ cin>>x;
+ cout<<"enter the secend number :"<<endl;
+ cin>>y;
+ cout<<"enter the last number :"<<endl;
+ cin>>z;
+ cout<<"the sum of the numbers is = "<<""<<x+y+z<<endl;
+ return 0;
+
+
+}
