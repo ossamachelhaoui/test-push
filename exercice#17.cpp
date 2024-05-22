39cf173a3158352d606ea255e3389a1e04feb640
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 12:29:34 +0100

    triangle area

---
+++ exercice#17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int h;
+    int result;
+
+      cout<<"enter a :"<<endl;
+      cin>>a;
+      cout<<"enter h :"<<endl;
+      cin>>h;
+
+      result=  a/2 * h;
+
+      cout<<result<<endl;
+}
