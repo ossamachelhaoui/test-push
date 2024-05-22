fdf346bc0b5d37012b5280ac2693dee9141f51e7
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 12:07:36 +0100

    #10

---
+++ exercice#10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+
+ cout<<"enter the first number :"<<endl;
+ cin>>a;
+ cout<<"enter the secend number :"<<endl;
+ cin>>b;
+ cout<<"enter the last number :"<<endl;
+ cin>>c;
+ cout<<"the avg is ="<<" "<<(a+b+c)/3;
+ return 0;
+
+
+}
