7070d034922e75afbc9484cf9b9a73ac4faa3f0a
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 11:52:50 +0100

    #7

---
+++ exercice#7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int x;
+
+ cout<<"enter number"<<endl;
+ cin>>x;
+ cout<<"half of the number is ="<<x / 2<<endl;
+ return 0;
+
+
+}
