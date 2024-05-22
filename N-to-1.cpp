eaecd34ecbb34ac30044d31db6fa7be3f9307d9b
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 19:21:37 +0100

    N-to-1

---
+++ N-to-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+
+    cout<<"enter n :";
+    cin>>n;
+
+    for (int i=n; i>=0 ; i--){
+      cout<<i<<endl;
+    }
+    
+
+}
