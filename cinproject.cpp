0f094b2e6d3fe51fb4c705828a16e10cba395b55
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 11:39:11 +0100

    cinproject

---
+++ cinproject.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a;
+    int b;
+
+    cout<<"pleas enter first number :";
+    cin>>a;
+    cout<<"pleas enter secend number :";
+    cin>>b;
+
+    cout<<"a + B ="<<a+b<<endl;
+    cout<<"a - B ="<<a-b<<endl;
+    cout<<"a / B ="<<a/b<<endl;
+    cout<<"a * B ="<<a*b<<endl;
+    cout<<"a % B ="<<a%b<<endl;
+
+    return 0;
+}
