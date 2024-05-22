92fc5811fbbc20e11dfaa450273399b7a3553cf3
ossama <ossamachalhaoui1@gmail.com>
2024-05-22 12:42:46 +0100

    circle area trough diameter

---
+++ exercice#19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int d;
+    float result;
+    float pi=3.14;
+
+      cout<<"enter d :"<<endl;
+      cin>>d;
+
+      result= pi*(d*d)/4 ;
+
+      cout<<result<<endl;
+}
