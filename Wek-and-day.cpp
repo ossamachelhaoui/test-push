0b14bc8764542b5cc6956bdc10eb2fa4be9a3181
ossama <ossamachalhaoui1@gmail.com>
2024-05-28 10:06:26 +0100

    Wek-and-day

---
+++ Wek-and-day.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    int week;
+    int day;
+
+    cout<<"enter number of hours : ";
+    cin>>n;
+
+    day=n/24;
+    week=day/7;
+    cout<<"the numbers of day is :"<<day<<endl;
+    cout<<"the numbers of week is :"<<week<<endl;
+
+
+}
